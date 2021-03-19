0ef81887fc6368d86aada34b7a8a239491262e77
Kishore Kumar Pechetti <v-kipec@microsoft.com>
2021-03-19 13:38:26 -0700

    Completed Routing functionality

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import {Link} from 'react-router-dom';
+
+const PageNotFound = () => {
+    return(
+            <p>
+                Page Not found. Go to <Link to="/">Home</Link>
+            </p>
+    )
+}
+
+export default PageNotFound;
