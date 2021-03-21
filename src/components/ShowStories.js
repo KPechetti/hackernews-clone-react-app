6446b636be125d5c187052d7167b337776cb3bfe
Kishore Kumar Pechetti <v-kipec@microsoft.com>
2021-03-20 17:08:06 -0700

    Added Loaing Overlay

--- src/components/ShowStories.js
+++ src/components/ShowStories.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import Story from './Story';
 import useDataFetcher from '../hooks/dataFetcher';
+import Loader from './Loader';
 
 const ShowStories = ({type}) => {
   const { isLoading, stories } = useDataFetcher(type? type : 'top');
 
   return (
     <React.Fragment>
-      {isLoading ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <React.Fragment>
-          {stories.map(({ data: story }) => (
-            <Story key={story.id} story={story} />
-          ))}
-        </React.Fragment>
-      )}
+      <Loader show={isLoading}>Loading...</Loader>
+      <React.Fragment>
+        {stories.map(({ data: story }) => (
+          <Story key={story.id} story={story} />
+        ))}
+      </React.Fragment>
     </React.Fragment>
   );
+
 };
 
 export default ShowStories;
